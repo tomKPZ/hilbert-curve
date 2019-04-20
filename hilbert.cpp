ec67ac9c8a1fd6a13f7b3472981640fe071efc3b
tomKPZ <tomKPZ@gmail.com>
2019-04-19 23:16:53 -0700

    Update hilbert.cpp

--- hilbert.cpp
+++ hilbert.cpp
@@ -3,16 +3,16 @@
 #include <memory>
 
 int main(void) {
+  using Int = short;
   constexpr unsigned int N = 2;
   constexpr unsigned int K = 14;
 
-  auto curve = std::make_unique<std::array<short, N>[]>(1 << N * K);
-  Hilbert<short, unsigned int>::Curve<N, K>(curve[0].data());
-  // Hilbert<short, unsigned int>::Curve(N, K, curve[0].data());
+  auto curve = std::make_unique<std::array<Int, N>[]>(1 << N * K);
+  Hilbert<Int, unsigned int>::Curve(N, K, curve[0].data());
 
-  // short v[N];
+  // Int v[N];
   // for (unsigned int i = 0; i < 1 << N * K; i++) {
-  //   Hilbert<short, unsigned int>::IToV(N, K, i, v);
+  //   Hilbert<Int, unsigned int>::IToV(N, K, i, v);
   // }
   return 0;
 }
