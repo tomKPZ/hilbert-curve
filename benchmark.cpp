d5e8676dd59c4727cb770f393244318236ed7f4b
tomKPZ <tomKPZ@gmail.com>
2024-02-27 10:32:15 -0800

    Bring back IsToVs and VsToIs since they are faster

--- benchmark.cpp
+++ benchmark.cpp
@@ -9,12 +9,12 @@
 int main() {
   using ITy = std::uint32_t;
   using ViTy = std::uint16_t;
-  using STy = unsigned int;
 
-  constexpr STy N = 2;
-  constexpr STy K = 14;
+  constexpr uint N = 2;
+  constexpr uint K = 14;
 
-  auto time_impl = [&](STy bytes, const char* desc, auto&& f) {
+  constexpr uint bytes = sizeof(ViTy) * N << N * K;
+  auto time = [&](const char* desc, auto&& f) {
     auto start = std::chrono::high_resolution_clock::now();
     f();
     auto duration = std::chrono::high_resolution_clock::now() - start;
@@ -23,23 +23,16 @@ int main() {
     std::cout << desc << ":\t" << count << " s (" << throughput << " GiB/s)\n";
   };
 
-  constexpr STy bytes = sizeof(ViTy) * N << N * K;
-  auto time = [&](const char* desc, auto&& f) { time_impl(bytes, desc, f); };
   std::unique_ptr<ViTy[]> vs;
   time("new[]", [&]() { vs = std::make_unique<ViTy[]>(N << N * K); });
   // memset() is not necessary, but just gives a frame of reference
   // for how fast we can sequentially write to main memory.
   time("memset", [&]() { std::memset(vs.get(), 0, bytes); });
-  time("IsToVs", [&]() {
-    for (ITy i = 0; i < 1U << (N * K); i++) {
-      Hilbert<N, K, ViTy, ITy>::IToV(i, &vs[N * i]);
-    }
-  });
-  time("VsToIs", [&]() {
-    for (ITy i = 0; i < 1U << (N * K); i++) {
-      Hilbert<N, K, ViTy, ITy>::VToI(&vs[N * i]);
-    }
-  });
+  time("IsToVs", [&]() { Hilbert<N, K, ViTy, ITy>::IsToVs(vs.get()); });
+  std::unique_ptr<ITy[]> is;
+  time("new[]", [&]() { is = std::make_unique<ITy[]>(1 << N * K); });
+  time("memset", [&]() { std::memset(vs.get(), 0, bytes); });
+  time("VsToIs", [&]() { Hilbert<N, K, ViTy, ITy>::VsToIs(is.get()); });
 
   return 0;
 }
--- hilbert.hpp
+++ hilbert.hpp
@@ -27,6 +27,108 @@ struct Hilbert {
 
   Hilbert() = delete;
 
+  static constexpr void IsToVs(ViTy vs[]) {
+    for (uint i = 0; i < N; ++i) {
+      vs[i] = 0;
+    }
+    if (N == 0 || K == 0) {
+      return;
+    }
+    for (uint k = 0; k < K; ++k) {
+      for (uint i = 1; i < (1U << N); ++i) {
+        uint rotate = N - 1;
+        if (i != (1U << N) - 1) {
+          uint j = (i - 1) >> 1;
+          for (uint bits = ~j & (j + 1); bits != 0; bits >>= 1) {
+            --rotate;
+          }
+        }
+
+        uint gray = ((i - 1) >> 1) ^ (i - 1);
+        for (uint p = 0; p < 1U << N * k; ++p) {
+          uint write_base = N * ((i << N * k) + p);
+          uint read_base = N * p;
+          bool reflect = !((i + 1) & 2);
+          for (uint j = 0; j < N; ++j) {
+            uint order = rotate + j >= N ? rotate + j - N : rotate + j;
+            uint coord = (i + (1U << j)) & (1U << (j + 1));
+            ViTy offset = coord ? (1U << k) : 0;
+            ViTy temp = vs[read_base + order];
+            temp = reflect ? ~temp & ((1U << k) - 1) : temp;
+            vs[write_base + j] = temp + offset;
+            reflect = gray & (1U << (j + 1));
+          }
+        }
+      }
+
+      for (uint p = 0; p < 1U << N * k; ++p) {
+        ViTy temp = vs[N * (p + 1) - 1];
+        for (uint i = N - 1; i > 0; --i) {
+          vs[N * p + i] = vs[N * p + i - 1];
+        }
+        vs[N * p] = temp;
+      }
+    }
+  }
+
+  static constexpr void VsToIs(ITy is[]) {
+    is[0] = 0;
+    if (N == 0 || K == 0) {
+      return;
+    }
+    for (uint k = 0; k < K; ++k) {
+      for (uint j = 0; j < (1U << (N * k)); ++j) {
+        is[j | (1U << (N * (k + 1) - 1))] = is[j];
+      }
+      for (uint j = 0; j < (1U << (N * k)); ++j) {
+        ITy dest = 0;
+        for (uint nvi = 0; nvi < N; ++nvi) {
+          uint dsh = (nvi == 0 ? N - 1 : nvi - 1) * k;
+          dest |= (((((1U << k) - 1) << dsh) & j) >> dsh) << (nvi * (k + 1));
+        }
+        is[dest] = is[j | (1U << (N * (k + 1) - 1))];
+      }
+
+      for (uint i = 1; i < (1U << N); ++i) {
+        ITy orthant = 0;
+        bool parity = 0;
+        for (uint j = 0; j < N; ++j) {
+          parity ^= (i & (1U << j)) >> j;
+          orthant |= parity << (N - j - 1);
+        }
+
+        uint rotate = N - 1;
+        if (orthant != 0 && orthant != (1U << N) - 1) {
+          ITy j = (orthant - 1) >> 1;
+          for (ITy bits = ~j & (j + 1); bits != 0; bits >>= 1) {
+            --rotate;
+          }
+        }
+
+        ITy gray = ((orthant - 1) >> 1) ^ (orthant - 1);
+        for (uint j = 0; j < (1U << (N * k)); ++j) {
+          ITy src = 0;
+          ITy dest = 0;
+          bool reflect = !(orthant == 0 || (orthant + 1) & 2);
+          for (uint nvi = 0; nvi < N; ++nvi) {
+            uint dsh = nvi + rotate;
+            dsh = (dsh >= N ? dsh - N : dsh) * k;
+            uint di = ((((1U << k) - 1) << dsh) & j) >> dsh;
+            di = reflect ? ~di & ((1U << k) - 1) : di;
+            di |= (i & (1U << (N - nvi - 1))) ? 1U << k : 0;
+            dest |= di << (nvi * (k + 1));
+
+            uint ssh = (nvi == 0 ? N - 1 : nvi - 1) * k;
+            src |= (((((1U << k) - 1) << ssh) & j) >> ssh) << (nvi * (k + 1));
+
+            reflect = gray & (1U << (nvi + 1));
+          }
+          is[dest] = is[src] + orthant * (1U << (N * k));
+        }
+      }
+    }
+  }
+
   static constexpr void Scatter(ITy i, ViTy v[]) {
     for (uint n = 0; n < N; ++n) {
       v[n] = 0;
@@ -39,7 +141,7 @@ struct Hilbert {
     }
   }
 
-  static constexpr ITy Gather(ViTy v[]) {
+  static constexpr ITy Gather(const ViTy v[]) {
     ITy i = 0;
     for (uint k = 0; k < K; ++k) {
       for (uint n = 0; n < N; ++n) {
